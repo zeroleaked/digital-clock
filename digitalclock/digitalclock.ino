9623e8860d8802c77035eed1b5e8325ede117902
zeroleaked <46643836+zeroleaked@users.noreply.github.com>
2021-02-02 18:34:22 +0700

    state button

--- digitalclock/digitalclock.ino
+++ digitalclock/digitalclock.ino
@@ -1,22 +1,23 @@
-#include <TM1637Display.h>
+
  
  // set current time by editing the values at line 16 and 17
-const int 7seg_clock = 7;
-const int 7seg_data = 8;
+const int sevseg_clock = 7;
+const int sevseg_data = 8;
 uint8_t digits[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f }; // decimal to 7 segment
 
 const int button0 = 3;
+int button0_clicked = 0;
 
 void setup()
 {
   Serial.begin(9600);
   setupInterrupt();
-  pinMode(7seg_clock, OUTPUT); // pin 7 is 7 seg clock
-  pinMode(7seg_data, OUTPUT); // pin 8 is 7 seg data
+  pinMode(sevseg_clock, OUTPUT); // pin 7 is 7 seg clock
+  pinMode(sevseg_data, OUTPUT); // pin 8 is 7 seg data
   start();
   writeValue(0x8c); // set 7 seg brightness
   stop();
-  7seg_write(0x00, 0x00, 0x00, 0x00); // clear 7 seg
+  sevseg_write(0x00, 0x00, 0x00, 0x00); // clear 7 seg
 
   pinMode(button0, INPUT);
 }
@@ -59,13 +60,16 @@ void loop()
   uint8_t minutes = (byte)((t / 60) % 60);
   uint8_t hours = (byte)((t / 3600) % 24);
   uint8_t seconds = (byte)(t % 60);
-  7seg_write(digits[hours / 10], digits[hours % 10] | ((seconds & 0x01) << 7), digits[minutes / 10], digits[minutes % 10]);
-  if (digitalRead(button0) == HIGH) {
-      Serial.write("clicked");
-    }
+  sevseg_write(digits[hours / 10], digits[hours % 10] | ((seconds & 0x01) << 7), digits[minutes / 10], digits[minutes % 10]);
+  if (digitalRead(button0) == HIGH && !button0_clicked) {
+      Serial.write("clicked\n");
+      button0_clicked = 1;
+  } else if (digitalRead(button0) == LOW) {
+      button0_clicked = 0;
+  }
 }
 
-void 7seg_write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
+void sevseg_write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
 {
   start();
   writeValue(0x40);
@@ -81,22 +85,22 @@ void 7seg_write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
 
 void start(void)
 {
-  digitalWrite(7seg_clock,HIGH);
-  digitalWrite(7seg_data,HIGH);
+  digitalWrite(sevseg_clock,HIGH);
+  digitalWrite(sevseg_data,HIGH);
   delayMicroseconds(5);
-  digitalWrite(7seg_data,LOW);
-  digitalWrite(7seg_clock,LOW);
+  digitalWrite(sevseg_data,LOW);
+  digitalWrite(sevseg_clock,LOW);
   delayMicroseconds(5);
 }
 
 void stop(void)
 {
-  digitalWrite(7seg_clock,LOW);
-  digitalWrite(7seg_data,LOW);
+  digitalWrite(sevseg_clock,LOW);
+  digitalWrite(sevseg_data,LOW);
   delayMicroseconds(5);
 
-  digitalWrite(7seg_clock,HIGH);
-  digitalWrite(7seg_data,HIGH);
+  digitalWrite(sevseg_clock,HIGH);
+  digitalWrite(sevseg_data,HIGH);
   delayMicroseconds(5);
 }
 
@@ -104,19 +108,19 @@ bool writeValue(uint8_t value)
 {
   for(uint8_t i = 0; i < 8; i++)
   {
-    digitalWrite(7seg_clock, LOW);
+    digitalWrite(sevseg_clock, LOW);
     delayMicroseconds(5);
-    digitalWrite(7seg_data, (value & (1 << i)) >> i);
+    digitalWrite(sevseg_data, (value & (1 << i)) >> i);
     delayMicroseconds(5);
-    digitalWrite(7seg_clock, HIGH);
+    digitalWrite(sevseg_clock, HIGH);
     delayMicroseconds(5);
   }
-  digitalWrite(7seg_clock,LOW);
+  digitalWrite(sevseg_clock,LOW);
   delayMicroseconds(5);
-  pinMode(7seg_data,INPUT);
-  digitalWrite(7seg_clock,HIGH);
+  pinMode(sevseg_data,INPUT);
+  digitalWrite(sevseg_clock,HIGH);
   delayMicroseconds(5);
-  bool ack = digitalRead(7seg_data) == 0;
-  pinMode(7seg_data,OUTPUT);
+  bool ack = digitalRead(sevseg_data) == 0;
+  pinMode(sevseg_data,OUTPUT);
   return ack;
 }
