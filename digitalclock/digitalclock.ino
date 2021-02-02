c825bfb7edbe53ca2522888fa65a7abe774c16af
zeroleaked <46643836+zeroleaked@users.noreply.github.com>
2021-02-02 18:06:47 +0700

    pushbutton

--- digitalclock/digitalclock.ino
+++ digitalclock/digitalclock.ino
@@ -1,17 +1,24 @@
+#include <TM1637Display.h>
+ 
  // set current time by editing the values at line 16 and 17
-const int clock = 7;
-const int data = 8;
+const int 7seg_clock = 7;
+const int 7seg_data = 8;
 uint8_t digits[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f }; // decimal to 7 segment
+
+const int button0 = 3;
 
 void setup()
 {
+  Serial.begin(9600);
   setupInterrupt();
-  pinMode(clock, OUTPUT); // pin 7 is 7 seg clock
-  pinMode(data, OUTPUT); // pin 8 is 7 seg data
+  pinMode(7seg_clock, OUTPUT); // pin 7 is 7 seg clock
+  pinMode(7seg_data, OUTPUT); // pin 8 is 7 seg data
   start();
   writeValue(0x8c); // set 7 seg brightness
   stop();
-  write(0x00, 0x00, 0x00, 0x00); // clear 7 seg
+  7seg_write(0x00, 0x00, 0x00, 0x00); // clear 7 seg
+
+  pinMode(button0, INPUT);
 }
 
 byte tcnt2;
@@ -33,7 +40,7 @@ void setupInterrupt()
   // prescaler set to 1024
   TCCR2B |= (1<<CS22)  | (1<<CS20); 
   TCCR2B &= ~(1<<CS21);
-  // start from 131
+  // start from 131 (125 ticks to overflow)
   tcnt2 = 131;
   TCNT2 = tcnt2;
   // enable overflow interrupt
@@ -52,10 +59,13 @@ void loop()
   uint8_t minutes = (byte)((t / 60) % 60);
   uint8_t hours = (byte)((t / 3600) % 24);
   uint8_t seconds = (byte)(t % 60);
-  write(digits[hours / 10], digits[hours % 10] | ((seconds & 0x01) << 7), digits[minutes / 10], digits[minutes % 10]);
+  7seg_write(digits[hours / 10], digits[hours % 10] | ((seconds & 0x01) << 7), digits[minutes / 10], digits[minutes % 10]);
+  if (digitalRead(button0) == HIGH) {
+      Serial.write("clicked");
+    }
 }
 
-void write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
+void 7seg_write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
 {
   start();
   writeValue(0x40);
@@ -71,47 +81,42 @@ void write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
 
 void start(void)
 {
-  digitalWrite(clock,HIGH);
-  digitalWrite(data,HIGH);
+  digitalWrite(7seg_clock,HIGH);
+  digitalWrite(7seg_data,HIGH);
   delayMicroseconds(5);
-  digitalWrite(data,LOW);
-  digitalWrite(clock,LOW);
+  digitalWrite(7seg_data,LOW);
+  digitalWrite(7seg_clock,LOW);
   delayMicroseconds(5);
 }
 
 void stop(void)
 {
-  digitalWrite(clock,LOW);
-  digitalWrite(data,LOW);
+  digitalWrite(7seg_clock,LOW);
+  digitalWrite(7seg_data,LOW);
   delayMicroseconds(5);
 
-  digitalWrite(clock,HIGH);
-  digitalWrite(data,HIGH);
+  digitalWrite(7seg_clock,HIGH);
+  digitalWrite(7seg_data,HIGH);
   delayMicroseconds(5);
 }
 
 bool writeValue(uint8_t value)
 {
-  Serial.print("WRITING ");
-  Serial.print(value);
-  Serial.print("\n");
   for(uint8_t i = 0; i < 8; i++)
   {
-    digitalWrite(clock, LOW);
+    digitalWrite(7seg_clock, LOW);
     delayMicroseconds(5);
-    digitalWrite(data, (value & (1 << i)) >> i);
-    Serial.print((value & (1 << i)) >> i);
-    Serial.print("\n");
+    digitalWrite(7seg_data, (value & (1 << i)) >> i);
     delayMicroseconds(5);
-    digitalWrite(clock, HIGH);
+    digitalWrite(7seg_clock, HIGH);
     delayMicroseconds(5);
   }
-  digitalWrite(clock,LOW);
+  digitalWrite(7seg_clock,LOW);
   delayMicroseconds(5);
-  pinMode(data,INPUT);
-  digitalWrite(clock,HIGH);
+  pinMode(7seg_data,INPUT);
+  digitalWrite(7seg_clock,HIGH);
   delayMicroseconds(5);
-  bool ack = digitalRead(data) == 0;
-  pinMode(data,OUTPUT);
+  bool ack = digitalRead(7seg_data) == 0;
+  pinMode(7seg_data,OUTPUT);
   return ack;
 }
