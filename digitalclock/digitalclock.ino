76e9643707862e77957df2541724317c773cfe2c
zeroleaked <46643836+zeroleaked@users.noreply.github.com>
2021-02-02 22:05:40 +0700

    sev seg off

--- digitalclock/digitalclock.ino
+++ digitalclock/digitalclock.ino
@@ -1,11 +1,12 @@
+#include <TM1637Display.h>
 
- 
- // set current time by editing the values at line 16 and 17
-const int sevseg_clock = 7;
-const int sevseg_data = 8;
+const int sevseg_clock = 7, sevseg_data = 8;
 uint8_t digits[] = { 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f }; // decimal to 7 segment
 
-const int button0 = 3;
+TM1637Display sevseg(sevseg_clock, sevseg_data);
+
+const int button0 = 6, button1 = 5, button2 = 4;
+
 int button0_clicked = 0;
 
 void setup()
@@ -14,18 +15,19 @@ void setup()
   setupInterrupt();
   pinMode(sevseg_clock, OUTPUT); // pin 7 is 7 seg clock
   pinMode(sevseg_data, OUTPUT); // pin 8 is 7 seg data
-  start();
-  writeValue(0x8c); // set 7 seg brightness
-  stop();
-  sevseg_write(0x00, 0x00, 0x00, 0x00); // clear 7 seg
+  sevseg.clear();
+  sevseg.setBrightness(7);
 
   pinMode(button0, INPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  Serial.println("setuping..");
 }
 
 byte tcnt2;
 // initial time
-unsigned long int setMinutes = 06;
-unsigned long int setHours = 17;
+unsigned long int setMinutes = 52;
+unsigned long int setHours = 19;
 unsigned long time = (setMinutes * 60 * 1000) + (setHours * 3600 *1000); // in miliseconds
   
 void setupInterrupt()
@@ -60,71 +62,86 @@ void loop()
   uint8_t minutes = (byte)((t / 60) % 60);
   uint8_t hours = (byte)((t / 3600) % 24);
   uint8_t seconds = (byte)(t % 60);
-  sevseg_write(digits[hours / 10], digits[hours % 10] | ((seconds & 0x01) << 7), digits[minutes / 10], digits[minutes % 10]);
-  buttons_check();
+  uint8_t dec = (byte) (hours * 100 + minutes);
+  sevseg.showNumberDecEx(minutes, 0, true, 2, 2);
+  sevseg.showNumberDecEx(hours, (0x80 >> seconds % 2), true, 2, 0);
+  bool setting_button_clicked = buttons_check();
+  if (setting_button_clicked) Serial.println("click");
+//  if (setting_button_clicked) button_set_time(hours, minutes);
+  delay(100);
 }
 
-void buttons_check() {
+bool buttons_check() {
   if (digitalRead(button0) == HIGH && !button0_clicked) {
-      Serial.write("clicked\n");
       button0_clicked = 1;
+      return true;
   } else if (digitalRead(button0) == LOW) {
       button0_clicked = 0;
-  }
+      return false;
+  } else return false;
 }
 
-void sevseg_write(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
-{
-  start();
-  writeValue(0x40);
-  stop();
-  start();
-  writeValue(0xc0);
-  writeValue(first);
-  writeValue(second);
-  writeValue(third);
-  writeValue(fourth);
-  stop();
+void button_set_time(uint8_t hours, uint8_t minutes) {
+  uint8_t values[] = { hours, minutes };
+  uint8_t pointer = 0;
+  uint8_t zero[] = { 0, 0x80 };
+  uint8_t count = 0;
+  while(1) {
+    // blink every 900 ms
+    if (count % 3 == 0) {
+      if (count % 6 == 0) {
+        sevseg.setSegments(zero, 2, pointer*2);
+      }
+      else {
+        sevseg.showNumberDecEx(values[pointer], 0x40, true, 2, pointer*2);
+      }
+    }
+
+    // increment hours
+    if (digitalRead(button1) == HIGH) {
+      if (pointer == 0) {
+        if (values[0] < 23) values[0]++;
+        else values[0] = 0;
+      }
+      else if (pointer == 1) {
+        if (values[1] < 59) values[1]++;
+        else values[1] = 0;
+      }
+      sevseg.showNumberDecEx(values[pointer], 0x40, true, 2, pointer*2);
+    }
+
+    // decrement hours
+    if (digitalRead(button2) == HIGH) {
+      if (pointer == 0) {
+        if (values[0] > 0) values[0]--;
+        else values[0] = 23;
+      }
+      else if (pointer == 1) {
+        if (values[1] > 0) values[1]--;
+        else values[1] = 59;
+      }
+      sevseg.showNumberDecEx(values[pointer], 0x40, true, 2, pointer*2);
+    }
+    
+    count ++;
+    delay(150);
+
+    // change pointer
+    if (digitalRead(button0) == HIGH) {
+        sevseg.showNumberDecEx(values[pointer], 0x40, true, 2, pointer*2);
+        if (pointer < 1) pointer++;
+        else {
+//          change_time(values);
+          Serial.write("check");
+          Serial.println(time);
+          return;
+        }
+    }
+  };
 }
 
-void start(void)
-{
-  digitalWrite(sevseg_clock,HIGH);
-  digitalWrite(sevseg_data,HIGH);
-  delayMicroseconds(5);
-  digitalWrite(sevseg_data,LOW);
-  digitalWrite(sevseg_clock,LOW);
-  delayMicroseconds(5);
-}
-
-void stop(void)
-{
-  digitalWrite(sevseg_clock,LOW);
-  digitalWrite(sevseg_data,LOW);
-  delayMicroseconds(5);
-
-  digitalWrite(sevseg_clock,HIGH);
-  digitalWrite(sevseg_data,HIGH);
-  delayMicroseconds(5);
-}
-
-bool writeValue(uint8_t value)
-{
-  for(uint8_t i = 0; i < 8; i++)
-  {
-    digitalWrite(sevseg_clock, LOW);
-    delayMicroseconds(5);
-    digitalWrite(sevseg_data, (value & (1 << i)) >> i);
-    delayMicroseconds(5);
-    digitalWrite(sevseg_clock, HIGH);
-    delayMicroseconds(5);
-  }
-  digitalWrite(sevseg_clock,LOW);
-  delayMicroseconds(5);
-  pinMode(sevseg_data,INPUT);
-  digitalWrite(sevseg_clock,HIGH);
-  delayMicroseconds(5);
-  bool ack = digitalRead(sevseg_data) == 0;
-  pinMode(sevseg_data,OUTPUT);
-  return ack;
+void change_time(const uint8_t values[]) {
+  time = (values[1] * 60 * 1000) + (values[0] * 3600 *1000);
+  Serial.write("count ");
+          Serial.println(values[1] * 60 * 1000) + (values[0] * 3600 *1000);
 }
